69eb5c7a232bccd462a6e56cabb7bfb9cac15ffa
Ameya Joshi <ameya.j@amazatic.com>
2018-07-31 14:29:57 +0530

    Migration files created

--- src/core/settings.py
+++ src/core/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'online_shopping',
-        'USER': 'sumit',
-        'PASSWORD': 's',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '',
     }

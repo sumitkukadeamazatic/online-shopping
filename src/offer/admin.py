efbd9153a41e2cb7675f97a366bdbd189070fefb
dnyaneshwar <mahajandi927@gmail.com>
2018-08-01 11:34:10 +0530

    changes in addmin.py and add OfferLineitem

--- src/offer/admin.py
+++ src/offer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.Offer)
+admin.site.register(models.OrderOffer)
+admin.site.register(models.ProductOffer)
+admin.site.register(models.UserOffer)
+admin.site.register(models.OfferLineitem)
--- src/order/admin.py
+++ src/order/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.Order)
+admin.site.register(models.Cart)
+admin.site.register(models.CartProduct)
+admin.site.register(models.Lineitem)
+admin.site.register(models.LineitemTax)
+admin.site.register(models.LineShippingDetails)
+admin.site.register(models.OrderLog)
+admin.site.register(models.PaymentMethod)
+admin.site.register(models.ShippingDetails)

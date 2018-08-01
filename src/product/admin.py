91c8a599fb9db234fab1000db6f5504324e8ff64
sumit <sumit.k@amazatic.com>
2018-08-01 15:52:09 +0530

    views and minor changes

--- src/product/admin.py
+++ src/product/admin.py
@@ -2,53 +2,52 @@ from django.contrib import admin
 from . import models
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name','slug','created_at','updated_at']
+    list_display = ['id','name','slug','created_at','updated_at']
     prepopulated_fields = {'slug': ('name',)}
 
 
 class FeatureAdmin(admin.ModelAdmin) :
-    list_display = ['name','slug','created_at','updated_at']
+    list_display = ['id','name','slug','created_at','updated_at']
     prepopulated_fields = {'slug': ('name',)}
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','slug','description','base_price','selling_price','created_at','updated_at','images']
+    list_display = ['id','name','slug','description','base_price','selling_price','created_at','updated_at','images']
     list_filter = ['created_at','updated_at']
     list_editable = ['base_price','selling_price','images']
     prepopulated_fields = {'slug': ('name',)}
 
 
 class TaxAdmin(admin.ModelAdmin):
-    list_display = ['name','slug','percent','is_active','created_at','updated_at']
+    list_display = ['id','name','slug','percent','is_active','created_at','updated_at']
     list_filter = ['percent','is_active','created_at','updated_at']
     list_editable = ['name','percent']
     prepopulated_fields = {'slug': ('name',)}
 
 
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ['name','slug','created_at','updated_at']
+    list_display = ['id','name','slug','created_at','updated_at']
     list_editable = ['name']
     prepopulated_fields = {'slug': ('name',)}
 
 class ProductSellerAdmin(admin.ModelAdmin):
-    list_display = ['quantity','discount','min_delivery_days','max_delivery_days','available_pin_codes','is_default','created_at','updated_at']
+    list_display = ['id','quantity','discount','min_delivery_days','max_delivery_days','available_pin_codes','is_default','created_at','updated_at']
     list_filter = ['quantity','discount','min_delivery_days','max_delivery_days','created_at','updated_at']
     list_editable = ['quantity','discount','min_delivery_days','max_delivery_days']
-    prepopulated_fields = {'slug': ('name',)}
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['user','seller','product','rating','title','description']
+    list_display = ['id','user','seller','product','rating','title','description']
     list_filter = ['rating','created_at','updated_at']
 
 
 class WishlistAdmin(admin.ModelAdmin):
-    list_display = ['user','product']
+    list_display = ['id','user','product']
 
 
 admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.Feature, FeatureAdmin)
 admin.site.register(models.Tax, TaxAdmin)
-admin.site.register(models.CategoryTax, CategoryAdmin)
+admin.site.register(models.CategoryTax)
 admin.site.register(models.Brand, BrandAdmin)
 admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductFeature)

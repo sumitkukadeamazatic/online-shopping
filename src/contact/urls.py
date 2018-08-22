cf941ea4e5bd625b9d18d3b45d66098e57f97753
dnyaneshwarmahajan <dnyaneshwar.m@amazatic.com>
2018-08-22 13:58:40 +0530

    create Adress API using generics

--- src/contact/urls.py
+++ src/contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^(?P<pk>[0-9]+)/$', views.AddressList.as_view(), name='AddressView'),
+    re_path(r'^$',views.AddressList.as_view(),name='AddressView'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.AddressDetail.as_view(), name='AddressView')
 ]
--- src/contact/views.py
+++ src/contact/views.py
@@ -3,17 +3,9 @@
 """
 from contact.models import Address
 from contact.serializers import AddressSerializer
-from rest_framework import generics, mixins
+from rest_framework import generics
 
-
-
-class AddressList(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
-    """
-    Address Vies is use to Create, Update, Delete adresses
-    """
-
-    serializer_class = AddressSerializer
-    
+class AddressList(generics.ListCreateAPIView):
     def get_queryset(self):
         """
         This view should return a list of all the purchases
@@ -22,14 +14,15 @@ class AddressList(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Destroy
         user = self.request.user
         return Address.objects.filter(user=user)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-        
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    serializer_class = AddressSerializer
 
-    def patch(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+class AddressDetail(generics.RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Address.objects.filter(user=user)
 
-    def delete(self, request, *args, **kwargs): 
-        return self.destroy(request, *args, **kwargs)
+    serializer_class = AddressSerializer

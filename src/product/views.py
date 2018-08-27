f530251e51842b7a03ad5cd58bddacf0d304bd65
sumit <sumit.k@amazatic.com>
2018-08-27 16:54:18 +0530

    paginator function added

--- src/product/views.py
+++ src/product/views.py
@@ -10,7 +10,39 @@ from rest_framework import permissions, authentication#, status
 from django.core.paginator import Paginator
 #from django.http import JsonResponse
 #from .serializers import CategorySerializer
-from .models import Category,Product
+from .models import Category, Product
+
+def add_peginator(results, requested_page_no, items_per_page):
+    '''
+    This function will return data with pagination
+    it will take list of json and return filterd result
+    with pagination of json type
+    '''
+    requested_page_no = int(requested_page_no)
+    data = {}
+    page = {}
+    results_paginator = Paginator(results, items_per_page)
+    page_paginator = results_paginator.get_page(requested_page_no)
+    no_of_pages = results_paginator.num_pages
+
+
+    page['current_page'] = requested_page_no if requested_page_no <= no_of_pages and requested_page_no > 0 else "Invalid page number"
+    page['items_per_page'] = items_per_page
+    page['no_of_pages'] = no_of_pages
+    page['has_previous'] = page_paginator.has_previous()
+    page['has_next'] = page_paginator.has_next()
+    data['page'] = page
+
+    if not requested_page_no:
+        data['results'] = results_paginator.object_list
+    else:
+        data['results'] = page_paginator.object_list
+
+    return data
+
+
+
+
 
 class CategoryView(APIView):
     '''
@@ -25,7 +57,10 @@ class CategoryView(APIView):
         """
         GET all category from db
         """
-        requested_page_no = int(request.GET.get('page', False))
+        try:
+            requested_page_no = int(request.GET.get('page', False))
+        except ValueError:
+            requested_page_no = False
         requested_category_slug = request.GET.get('category_slug', None)
         items_per_page = 10
         parentid = list(Category.objects.values_list('id', flat=True).filter(
@@ -48,6 +83,7 @@ class CategoryView(APIView):
             results = list(Category.objects.values('id',
                                                    'name',
                                                    'slug').filter(parent_id=parentid))
+        #return Response(add_peginator(results, requested_page_no, items_per_page))
         results_paginator = Paginator(results, items_per_page)
         page_paginator = results_paginator.get_page(requested_page_no)
         no_of_pages = results_paginator.num_pages
@@ -56,7 +92,7 @@ class CategoryView(APIView):
         else:
             results = results_paginator.get_page(requested_page_no).object_list
         page = {}
-        page['current_page'] = requested_page_no if requested_page_no < no_of_pages and requested_page_no > 0 else "Invalid page number"
+        page['current_page'] = requested_page_no if requested_page_no <= no_of_pages and requested_page_no > 0 else "Invalid page number"
         page['items_per_page'] = items_per_page
         page['no_of_pages'] = no_of_pages
         page['has_previous'] = page_paginator.has_previous()
@@ -85,14 +121,19 @@ class ProductView(APIView):
         discount = request.data.get('discount', r'.*')
         feature_slug = request.data.get('feature_slug', r'.*')
         rating = request.data.get('rating', r'.*')
-        
         # Fetching category id from category slug
         category_id = list(Category.objects.values_list('id', flat=True).filter(
             slug=category_slug))
         category_id = category_id[0] if len(category_id) == 1 else None
-
+        # product id list 
+        #product_id_list = list(models.Product.objects.values_list("id",flat=True))
+        product_id_list = list(Product.objects.values_list('id',flat=True).filter(category_id=category_id))
+        
+        filter_flag = True
+        for pro_id in product_id_list:
+            if category_slug == r'.*':
+                pass
         # Fetching brand id from 
-
         products = list(Product.objects.values(
             "brand",
             "category",
@@ -102,10 +143,8 @@ class ProductView(APIView):
             "selling_price",
             "slug",
             "images"
-            ).filter(id__regex))
+            ).filter())
         #print(products[0])
-
-
 
 
         return Response({"products":products})

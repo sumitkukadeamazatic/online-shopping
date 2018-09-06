25031e2504fdb89e3716332c3672fafb0d2f4f56
sumit <sumit.k@amazatic.com>
2018-09-06 15:37:36 +0530

    error because of migration resolved

--- src/product/urls.py
+++ src/product/urls.py
@@ -7,11 +7,11 @@ from .views import CategoryView, ProductView, ProductSellerView, WishlistViewset
 from rest_framework.routers import DefaultRouter
 
 
-router = DefaultRouter()
-router.register('wishlist', WishlistViewset, base_name='wishlist')
-urlpatterns = router.urls
+#router = DefaultRouter()
+#router.register('wishlist', WishlistViewset, base_name='wishlist')
+#urlpatterns = router.urls
 
-urlpatterns += [
+urlpatterns = [
     path('category', CategoryView.as_view()),
     path('', ProductView.as_view()),
     path('<int:id>', ProductView.as_view()),

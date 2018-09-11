5f13a81a762eb86678f4e8a5e5c077a7f6cece4f
Supriya <supriyaghangale@gmail.com>
2018-09-11 19:38:54 +0530

    Return Order API done. View Return Order in process

--- src/return/serializers.py
+++ src/return/serializers.py
@@ -1,38 +1,104 @@
 from rest_framework import serializers
-from .models import Order, Lineitem
+from .models import Order as ReturnOrder, Lineitem as ReturnLineitem, OrderLog as ReturnOrderLog
+from order.models import Order, Lineitem
+
+class ReturnOrderSerializer(serializers.ModelSerializer):
+    """
+        ReturnOrder Serializer
+    """
+
+    class Meta:
+        model = ReturnOrder
+        fields = (
+            'order',
+        )
+
+    def create(self, validated_data):
+        return ReturnOrder.objects.create(status=validated_data['order'].status, order_id=validated_data['order'].id)
+
+class ReturnOrderLogSerializer(serializers.ModelSerializer):
+    """
+        ReturnOrderLog Serializer
+    """
+
+    class Meta:
+        model = ReturnOrderLog
+        fields = (
+            'status',
+            'description',
+            'return_lineitem',
+        )
+
+# changes in order_lineitem (product_id, seller_id -> product_seller_id)
+
+class ReturnLineitemSerializer(serializers.ModelSerializer):
+    """
+        ReturnLineitem Serializer
+    """
+
+    class Meta:
+        model = ReturnLineitem
+        fields = (
+            'reason',
+            'description',
+        )
+
+    def create(self, validated_data):
+        print (self.context)
+        print (validated_data)
+        print (self.context['request'].data)
+        if not ReturnOrder.objects.filter(order_id=self.context['order']):
+            returnOrder_serializer = ReturnOrderSerializer(data=self.context)
+            if returnOrder_serializer.is_valid():
+                returnOrder_serializer.save()
+        lineitem_data = Lineitem.objects.filter(order=self.context['order'], product=self.context['product']).values().first()  # order_lineitem
+        return_order_id = ReturnOrder.objects.get(order=self.context['order'])
+        returnLineitem_serializer = ReturnLineitem.objects.create(quantity=lineitem_data['quantity'], reason=validated_data['reason'], description=validated_data['description'], lineitem_id=lineitem_data['id'], return_order=return_order_id, status=lineitem_data['status'])
+        returnOrderLog_data = {'status': returnLineitem_serializer.status,'description': returnLineitem_serializer.description,'return_lineitem': returnLineitem_serializer.id}
+        returnOrderLog_serializer = ReturnOrderLogSerializer(data=returnOrderLog_data)
+        if returnOrderLog_serializer.is_valid():
+            returnOrderLog_serializer.save()
+        return returnLineitem_serializer
+
 
 class ReturnSerializer(serializers.ModelSerializer):
     """
         Return Serializer
     """
 
+    '''
     class Meta:
-        model = Order
+        model = ReturnLineitem
         fields = (
-            'id',
-            'created_at',
-            'updated_at',
-            'status',
-            'order',
-        )
-
-
-class ReturnLineitemSerializer(serializers.ModelSerializer):
-    """
-        Return Lineitem Serializer
-    """
-
-    class Meta:
-        model = Lineitem
-        fields = (
-            'quantity',
             'reason',
             'description',
-            'status',
+        )
+    '''
+
+    #image = serializers.SerializerMethodField()
+    class Meta:
+        model = ReturnLineitem
+        fields = (
             'lineitem',
             'return_order',
+            'status',
+            'description',
+            'reason',
+            'quantity'
         )
 
     def create(self, validated_data):
-        print (validated_data)
-        return 
+        content = self.context['request'].data
+        if not ReturnOrder.objects.filter(order_id=content['order']):
+            returnOrder_serializer = ReturnOrderSerializer(data=content)
+            if returnOrder_serializer.is_valid():
+                returnOrder_serializer.save()
+        lineitem_data = Lineitem.objects.filter(order=content['order'], product=content['product']).values().first()
+        return_order_id = ReturnOrder.objects.get(order=content['order'])
+        returnLineitem_serializer = ReturnLineitem.objects.create(quantity=lineitem_data['quantity'], reason=validated_data['reason'], description=validated_data['description'], lineitem_id=lineitem_data['id'], return_order=return_order_id, status=lineitem_data['status'])
+        returnOrderLog_data = {'status': returnLineitem_serializer.status,'description': returnLineitem_serializer.description,'return_lineitem': returnLineitem_serializer.id}
+        returnOrderLog_serializer = ReturnOrderLogSerializer(data=returnOrderLog_data)
+        if returnOrderLog_serializer.is_valid():
+            returnOrderLog_serializer.save()
+        return returnLineitem_serializer
+
--- src/return/views.py
+++ src/return/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets, status
-from .models import Order
+from .models import Order as ReturnOrder
+from order.models import *
 from .serializers import *
 from rest_framework.response import Response
 
-class ReturnViewSet(viewsets.ViewSet):
+class ReturnViewSetPrevious(viewsets.ViewSet):
     """
         A ViewSet to create and list return order
     """
@@ -16,13 +17,46 @@ class ReturnViewSet(viewsets.ViewSet):
         serializer = ReturnSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    """
     def create(self, request):
-        if not Order.objects.filter(order_id=request.data['order']):
-            return_order_serializer = ReturnSerializer(data=request.data)
-            if return_order_serializer.is_valid():
-                return_order_serializer.save()
-        serializer = ReturnLineitemSerializer(data=request.data)
+        if not ReturnOrder.objects.filter(order_id=request.data['order']):
+            print ("Hii")
+            returnOrder_serializer = ReturnOrderSerializer(data=request.data)
+            if returnOrder_serializer.is_valid():
+                returnOrder_serializer.save()
+                return Response(returnOrder_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(returnOrder_serializer.errors)
+        return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+    """
+
+    def create(self, request):
+        serializer = ReturnLineitemSerializer(data=request.data, context = request.data) # context = {'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors)
+
+class ReturnViewSet(viewsets.ModelViewSet):
+    """
+        A viewset to create and list return order
+    """
+
+    queryset = ReturnOrder.objects.all()
+    serializer_class = ReturnSerializer
+
+    def get_queryset(self):
+        # print (ReturnLineitem.objects.filter(return_order_id=13))
+        # return ReturnLineitem.objects.filter(return_order_id=13)
+        cart_ids = Cart.objects.filter(user_id=11).values_list('id', flat=True)
+        print (cart_ids)
+        order_ids = Order.objects.filter(cart_id__in=cart_ids).values_list('id', flat=True)
+        print (order_ids)
+        returnOrder_ids = ReturnOrder.objects.filter(order_id__in=order_ids).values_list('id',flat=True)
+        print (returnOrder_ids)
+        returnLineitem_ids = ReturnLineitem.objects.filter(return_order_id__in=returnOrder_ids)
+        print (returnLineitem_ids)
+        print (ReturnLineitem.objects.all().count())
+        return ReturnLineitem.objects.all()
+
+    def get_paginated_response(self, obj):
+        return Response(obj)

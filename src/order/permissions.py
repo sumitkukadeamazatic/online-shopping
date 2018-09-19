0c37e888fec273ff89d87afd16d3207dc8cc9da1
dnyaneshwarmahajan <dnyaneshwar.m@amazatic.com>
2018-09-19 16:33:26 +0530

    created shipping api

--- src/order/permissions.py
+++ src/order/permissions.py
@@ -9,9 +9,11 @@ class UserAccessPermission(BasePermission):
     message = "Access Denied."
     
     def has_permission(self, request, view):
+        print(view.action)
         if not request.auth:
             return False
-        allowed_actions = ['create', 'list','payment', 'retrieve', 'shipping']
+        allowed_actions = ('create', 'list','payment', 'retrieve', 'shipping')
         if view.action in allowed_actions:
            return True
         return  False
+
